18211da16736e9f284fad0df90fef5f25b665a87
syahiedh1406 <the.forbidden1996@gmail.com>
2022-09-10 22:56:02 +0700

    add service create, getByid, put, and delete account

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router()
-const controller = require('../controller/jwt/account')
+const controller = require('../controller/index')
 
 
-router.get('/getName',controller.get)
+router.get('/getName',controller.jwt.get)
+router.get('/getAccountById/:id',controller.jwt.getById)
+router.put('/updateAccount/:id',controller.jwt.update)
+router.post('/createAccount',controller.jwt.post)
+router.delete('/deleteAccount/:id',controller.jwt.delete)
 
 module.exports = router
